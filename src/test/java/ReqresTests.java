0a4a00556f763f3bc0c7f7dabcdfbff89301f4e3
Anton Usik <moustacheby@gmail.com>
2021-08-10 12:51:43 +0300

    Initial commit

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,57 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.google.gson.Gson;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ReqresTests {
+
+  public static final String BASE_URI = "https://reqres.in/";
+
+  @Test
+  public void getListOfUsersTest() {
+    given().
+        baseUri(BASE_URI).
+        when().
+        get("/api/users?page=2").
+        then().
+        log().all().
+        statusCode(200);
+  }
+
+  @Test
+  public void getSingleUserTest() {
+    int id = 2;
+    given().
+        baseUri(BASE_URI).
+        when().
+        get(String.format("/api/users/%d", id)).
+        then().
+        log().all().
+        statusCode(200).
+        assertThat().body("data.id", equalTo(4));
+  }
+
+  @Test
+  public void createNewUserTest() {
+    Gson gson = new Gson();
+
+    User user = User.builder()
+        .name("John Doe")
+        .job("TESTER")
+        .build();
+
+    String response = given().
+        baseUri(BASE_URI).
+        when().
+        body(user).
+        post("api/users").
+        then().
+        log().all().
+        statusCode(201).
+        extract().asString();
+
+    Assert.assertEquals(gson.fromJson(response, User.class), user);
+  }
+}
